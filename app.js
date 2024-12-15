341acec3655414517110680acfc2da074be59aa5
Hasun-byte <55263161+Hasun-byte@users.noreply.github.com>
2024-12-15 09:42:34 +0700

    Update app.js

--- app.js
+++ app.js
@@ -210,7 +210,3 @@ const PORT = process.env.PORT || 4000;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 });
-
-setInterval(() => {
-    console.log('Keeping server alive...');
-}, 10 * 60 * 1000); // Setiap 10 menit
